93e567564170e58df724bef4c156f6ed112cb41c
Arashdeep Panesar <as2panes@uwaterloo.ca>
2019-03-20 18:52:01 -0400

    trace info

--- src/Components/RightBox/Subjects/Trace/TraceInfo/TraceInfo.js
+++ src/Components/RightBox/Subjects/Trace/TraceInfo/TraceInfo.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const TraceInfo = () => {
 	return (
-		<p></p>
+		<div className="bg-black p2 pushLeft">
+			<p>Definition:</p>
+			<p>Theorems: </p>
+			<p>Algebraic Examples:</p>
+		</div>
 	);
 }
 
