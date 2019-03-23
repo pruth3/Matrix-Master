39959e052aaaa21b52c65881743a0f34ec7d33a9
Arashdeep Panesar <as2panes@uwaterloo.ca>
2019-03-23 12:55:27 -0400

    changed title

--- src/Components/RightBox/Subjects/Contributors/Contributors.js
+++ src/Components/RightBox/Subjects/Contributors/Contributors.js
@@ -6,7 +6,9 @@ const Contributors = () => {
 		<div className="mainDiv">
 			<footer className="footStyle tc-l bg-center cover bg-black">
 				<div className="w-100 ph3 pv5 bg-black-80 blackShade">
-					TODO: CREATE CSS GRID TO DISPLAY GITHUB PROFILES OF CONTRIBUTORS
+					<h1 className="homeText">
+				  		Contributors
+				  	</h1>
 				</div>
 			</footer>
 		</div>
