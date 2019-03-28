e3b2533574df3ce63e37d887ed640c12a7086dfd
Arashdeep Panesar <as2panes@uwaterloo.ca>
2019-03-28 18:31:33 -0400

    style change

--- src/Components/RightBox/Subjects/LinMap/LinMap.js
+++ src/Components/RightBox/Subjects/LinMap/LinMap.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import {connect} from 'react-redux'; 
 //eslint-disable-next-line
 import tachyons from 'tachyons';
 import './LinMap.css'
