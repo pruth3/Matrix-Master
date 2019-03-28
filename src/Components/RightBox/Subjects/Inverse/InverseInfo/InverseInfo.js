5ed470d8427d9197902a0c084bd85c8721ee1752
Arashdeep Panesar <as2panes@uwaterloo.ca>
2019-03-28 17:53:38 -0400

    recovered file

---
+++ src/Components/RightBox/Subjects/Inverse/InverseInfo/InverseInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const InverseInfo = () => {
+	return (
+		<div className="bg-black p2 pushLeft">
+			<p>Definition:</p>
+			<p>Theorems: </p>
+			<p>Algebraic Examples:</p>
+		</div>
+	);
+}
+
+export default InverseInfo;
