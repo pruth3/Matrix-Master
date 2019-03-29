bb4b48487d67b27d2eaed3b3470ec020ed62d43e
Arashdeep Panesar <as2panes@uwaterloo.ca>
2019-03-29 09:08:41 -0400

    added all contributors

--- src/Components/RightBox/Subjects/Contributors/Contributors.js
+++ src/Components/RightBox/Subjects/Contributors/Contributors.js
@@ -9,11 +9,31 @@ const Contributors = () => {
 					<h1 className="homeText">
 				  		Contributors
 				  	</h1>
-				  	<a rel="noopener noreferrer" 
-				  	   target="_blank" 
-				  	   href="https://github.com/apanesar11"
-				  	   className="githubLink">apanesar11</a>
-				</div>
+				  	<a  rel="noopener noreferrer" 
+						target="_blank" 
+						href="https://github.com/apanesar11"
+						className="githubLink">apanesar11</a>
+					<a  rel="noopener noreferrer" 
+						target="_blank" 
+						href="https://github.com/pruth3"
+						className="githubLink">pruth3</a>
+					<a  rel="noopener noreferrer" 
+						target="_blank" 
+						href="https://github.com/DivneetSingh26"
+						className="githubLink">DivneetSingh26</a>
+					<a  rel="noopener noreferrer" 
+						target="_blank" 
+						href="https://github.com/manpreetx247"
+						className="githubLink">manpreetx247</a>
+					<a  rel="noopener noreferrer" 
+						target="_blank" 
+						href="https://github.com/manpreetx247"
+						className="githubLink">manpreetx247</a>
+					<a  rel="noopener noreferrer" 
+						target="_blank" 
+						href="https://github.com/GP146"
+						className="githubLink">GP146</a>
+				</div> 
 			</footer>
 		</div>
 	);
