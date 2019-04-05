c08e7111bec29bf5222f43a3b8febada05810067
Arashdeep Panesar <as2panes@uwaterloo.ca>
2019-04-05 08:26:10 -0400

    file not needed

--- src/Components/RightBox/Subjects/AreaPgram/AreaPgram.js
+++ src/Components/RightBox/Subjects/AreaPgram/AreaPgram.js
@@ -24,7 +24,7 @@ const mapStateToProps = (state) => {
 	}
 } 
 
-const mapDispatchToProps = (dispatch) => { 
+const mapDispatchToProps = (dispatch) => {
  	return {
  		setCreate: (event) => dispatch(
  			setCreatePgram(document.getElementById('rowsPgram').value)
