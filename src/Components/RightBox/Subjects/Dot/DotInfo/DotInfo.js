b23ac00eca51f02f26f91cc069cffff5378605a2
Arashdeep Panesar <as2panes@uwaterloo.ca>
2019-04-13 18:46:04 -0400

    added EQN

--- src/Components/RightBox/Subjects/Dot/DotInfo/DotInfo.js
+++ src/Components/RightBox/Subjects/Dot/DotInfo/DotInfo.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './DotInfo.css'
+import MathJax from 'react-mathjax2'
+
+const ascii = 'U = 1/(R_(si) + sum_(i=1)^n(s_n/lambda_n) + R_(se))'
 
 const DotInfo = () => {
 	return (
 		<div className="bg-black p2 pushLeft">
 			<p>Definition:</p>
 			<p>Theorems: </p>
-			<p>Algebraic Examples:</p>
+			<p>
+				Algebraic Examples:
+			</p>
+			<span style={{textAlign: 'center'}}>
+				<MathJax.Context input='ascii' >
+	                <div>
+	                    <MathJax.Node>{ascii}</MathJax.Node>
+	                </div>
+	            </MathJax.Context>
+            </span>
 		</div>
 	);
 }
