55e20fd99cf658ffe9f178455e7b4a3b4c6fc991
Arashdeep Panesar <as2panes@uwaterloo.ca>
2019-04-13 18:48:46 -0400

    eqn pgram

--- src/Components/RightBox/Subjects/AreaPgram/AreaPgramInfo/AreaPgramInfo.js
+++ src/Components/RightBox/Subjects/AreaPgram/AreaPgramInfo/AreaPgramInfo.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import MathJax from 'react-mathjax2'
+
+const ascii = 'U = 1/(R_(si) + sum_(i=1)^n(s_n/lambda_n) + R_(se))'
 
 const AreaPgramInfo = () => {
 	return (
 		<div className="bg-black p2 pushLeft">
 			<p>Definition:</p>
 			<p>Theorems: </p>
-			<p>Algebraic Examples:</p>
+			<p>
+				Algebraic Examples:
+			</p>
+			<span style={{textAlign: 'center'}}>
+				<MathJax.Context input='ascii' >
+	                <div>
+	                    <MathJax.Node>{ascii}</MathJax.Node>
+	                </div>
+	            </MathJax.Context>
+            </span>
 		</div>
 	);
-};
+}
 
 export default AreaPgramInfo;
